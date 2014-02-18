91fb0cf0a8ea18f37a163ba7ccd79395147553da
catonrug <aigars.kadikis@gmail.com>
2014-02-18 10:45:49 -0800

    Update remove-office-2010-welcome-screen.cmd

--- remove-office-2010-welcome-screen.cmd
+++ remove-office-2010-welcome-screen.cmd
@@ -1,4 +1,1 @@
-@echo off
-reg add "HKLM\Software\Policies\Microsoft\Office\14.0\Common\General" ^
-/v ShownFirstRunOptin /t REG_DWORD /d 1 /f
-pause
+@echo off & reg add "HKLM\Software\Policies\Microsoft\Office\14.0\Common\General" /v ShownFirstRunOptin /t REG_DWORD /d 1 /f & pause
